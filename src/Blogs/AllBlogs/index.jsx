c5e6c69fa9b060ca7498ec760eb5d0192fa1d6e0
Nick Bansal <extreme_wired@msn.com>
2019-06-02 16:26:05 +0100

    updated proptypes

--- src/Blogs/AllBlogs/index.jsx
+++ src/Blogs/AllBlogs/index.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import {
+  arrayOf, shape, string, instanceOf,
+} from 'prop-types';
 
 const AllBlogs = ({ blogs }) => (
   <div>
-    {blogs.map((blog) => {
-      console.log(blog);
-      return (
-        <div>
-          <h4>{blog.title}</h4>
-          <h4>{blog.body}</h4>
-          <h4>{blog.label}</h4>
-          <img src={blog.image} alt="cat" />
-        </div>
-      );
-    })}
+    {blogs.map(blog => (
+      <div key={blog.title}>
+        <h4>{blog.title}</h4>
+        <h4>{blog.body}</h4>
+        <h4>{blog.label}</h4>
+        <img src={blog.image} alt="cat" />
+      </div>
+    ))}
   </div>
 );
+
+AllBlogs.propTypes = {
+  blogs: arrayOf(shape({
+    title: string,
+    image: string,
+    body: string,
+    label: string,
+    created: instanceOf(Date),
+  })),
+};
+
+AllBlogs.defaultProps = {
+  blogs: [],
+};
 
 export default AllBlogs;
--- src/Blogs/index.jsx
+++ src/Blogs/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import autobind from 'react-autobind';
 import { getAllBlogs } from '../Utils';
 import TopNavigation from './TopNavigation';
-// import AllBlogs from './AllBlogs';
+import AllBlogs from './AllBlogs';
 import GlobalStyles from '../Components/GlobalStyles';
 
 class Blogs extends React.Component {
@@ -32,17 +32,7 @@ class Blogs extends React.Component {
       <React.Fragment>
         <GlobalStyles />
         <TopNavigation />
-        {allBlogs.map((blog) => {
-          console.log(blog);
-          return (
-            <div>
-              <h4>{blog.title}</h4>
-              <h4>{blog.body}</h4>
-              <h4>{blog.label}</h4>
-              <img src={blog.image} alt="cat" />
-            </div>
-          );
-        })}
+        <AllBlogs blogs={allBlogs} />
       </React.Fragment>
     );
   }
