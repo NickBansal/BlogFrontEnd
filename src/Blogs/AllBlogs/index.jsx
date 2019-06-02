c434f45a8572290ce5973ff55d8e6e2b6347d8c6
Nick Bansal <extreme_wired@msn.com>
2019-06-02 18:15:25 +0100

    worked on resizing

--- src/Blogs/AllBlogs/index.jsx
+++ src/Blogs/AllBlogs/index.jsx
@@ -3,6 +3,7 @@ import {
   arrayOf, shape, string,
 } from 'prop-types';
 import styled from 'styled-components';
+import { breakPoints } from '../../Components/StyleGuide';
 
 const EveryBlog = styled.div`
 margin-top: 30px;
@@ -10,22 +11,37 @@ margin-top: 30px;
 
 const IndividualBlog = styled.div`
 display: flex;
-width: 80%
+flex-direction: column;
+align-items: center;
+width: 90%
 justify-content: center;
 margin: 40px auto;
 max-width: 700px;
+
+@media (min-width: ${breakPoints.tablet}) {
+flex-direction: row;
+}
 `;
 
 const BlogInfo = styled.div`
 display: flex;
 flex-direction: column;
+
+@media (min-width: ${breakPoints.tablet}) {
 margin-left: 20px;
+}
 `;
 
 const Image = styled.img`
-width: 240px;
-height: 210px;
-margin-right: 20px;
+width: 80%;
+margin-bottom: 20px;
+
+@media (min-width: ${breakPoints.tablet}) {
+  flex-direction: row;
+  width: 250px;
+  height: 210px;
+  margin: 0 20px 0 0
+}
 `;
 
 const Title = styled.h3`
@@ -33,11 +49,21 @@ margin: 0;
 font-size: 23px;
 color: #577284;
 line-height: 1.4rem;
+text-align: center;
+
+@media (min-width: ${breakPoints.tablet}) {
+text-align: left;
+}
 `;
 
 const Body = styled.p`
 font-size: 15px;
 color: #577284;
+text-align: center;
+
+@media (min-width: ${breakPoints.tablet}) {
+text-align: left;
+}
 `;
 
 const AllBlogs = ({ blogs }) => (
---
+++ src/Components/StyleGuide/index.js
@@ -0,0 +1,7 @@
+export const breakPoints = {
+  mobile: '320px',
+  largeMobile: '420px',
+  tablet: '768px',
+  smallDesktop: '992px',
+  desktop: '1280px',
+};
