b928cf2ce83de2555a038adb2952a9baf9a6637c
Nick Bansal <extreme_wired@msn.com>
2019-06-15 13:20:55 +0100

    fixed single blog styling

--- src/Blogs/SingleBlog/index.jsx
+++ src/Blogs/SingleBlog/index.jsx
@@ -7,6 +7,7 @@ import { Title, Image } from 'Components/GlobalStyles';
 const BlogWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `
 
 const BlogTitle = styled(Title)`
