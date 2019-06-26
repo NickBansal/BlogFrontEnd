a7711904d96ba3720b9d0d6aee2bfcf1e9c0a2ca
Nick Bansal <extreme_wired@msn.com>
2019-06-26 16:29:05 +0100

    api calls using hooks

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GlobalStyle from 'Components/GlobalStyles';
 import { getAllBlogs } from 'Utils';
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const HomePage = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-  componentDidMount() {
-    getAllBlogs().then((blogs) => {
-      this.setState({
-        allBlogs: blogs[0],
-      });
-    }).catch((error) => {
-      this.setState({
-        error,
-      });
-    });
-  }
-
-  render() {
-    const { allBlogs, error } = this.state;
-    return (
-      <React.Fragment>
-        <GlobalStyle />
-        {allBlogs && <h1>Blogs</h1>}
-        {error && <h1>Error</h1>}
-      </React.Fragment>
-    );
-  }
-}
+  useEffect(() => {
+    getAllBlogs()
+      .then(blogs => setData(blogs[0]))
+      .catch(err => setError(err));
+  }, []);
+  console.log(data);
+  console.log(error);
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <h1>Blogs</h1>
+    </React.Fragment>
+  );
+};
 
 export default HomePage;
+
+
+// componentDidMount() {
+//   getAllBlogs().then((blogs) => {
+//     this.setState({
+//       allBlogs: blogs[0],
+//     });
+//   }).catch((error) => {
+//     this.setState({
+//       error,
+//     });
+//   });
+// }
