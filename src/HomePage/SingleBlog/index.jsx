cf22c403a555a766fea279be3e810abef2d68ed7
Nick Bansal <extreme_wired@msn.com>
2019-06-27 19:07:19 +0100

    hooked up the single blog to the API

--- src/HomePage/SingleBlog/index.jsx
+++ src/HomePage/SingleBlog/index.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { string } from 'prop-types';
+import styled from 'styled-components';
+import { getSingleBlog } from 'Utils';
+import { spacing } from 'Components/StyleGuide';
 
-const SingleBlog = (props) => {
-  console.log(props);
-  return <p>Single Blog</p>;
+const Loading = styled.h1`
+margin-top: ${spacing.s5};
+  display: flex;
+  justify-content: center;
+`;
+
+const SingleBlog = ({ id }) => {
+  const [blog, setBlog] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getSingleBlog(id)
+      .then((data) => {
+        setBlog(data[0]);
+        setLoading(false);
+      })
+      .catch(err => setError(err));
+  }, []);
+  return (
+    <div>
+      {loading && <Loading>Loading....</Loading>}
+      {error && <Loading>Loading....</Loading>}
+      <p>{blog.title}</p>
+      <img src={blog.image} alt="blog" />
+    </div>
+  );
+};
+
+SingleBlog.propTypes = {
+  id: string.isRequired,
 };
 
 export default SingleBlog;
