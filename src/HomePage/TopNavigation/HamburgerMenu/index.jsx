c50df1e72d6cca90ca68ff72f2ca0a36e2644f54
Nick Bansal <extreme_wired@msn.com>
2019-06-30 21:06:18 +0100

    redesigned the hamburger

--- src/HomePage/TopNavigation/HamburgerMenu/index.jsx
+++ src/HomePage/TopNavigation/HamburgerMenu/index.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
-import { colors, breakPoints } from 'Components/StyleGuide';
+import { colors, breakPoints, transitionSpeed } from 'Components/StyleGuide';
 
 const Line = styled.div`
-    width: 30px;
-    height: 4px;
-    background-color: ${colors.navText};
-    margin: 3px 0;
+  width: 30px;
+  height: 4px;
+  background-color: ${colors.navBackground};
+  margin: 3px 0;
 `;
 Line.displayName = 'Line';
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    border: 1px solid ${colors.navText};
-    justify-content: center;
-    margin: 4px 8px;
-    padding: 2px 6px;
-    border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid ${colors.navText};
+  justify-content: center;
+  margin: 4px 8px;
+  padding: 2px 6px;
+  border-radius: 10px;
+  background: ${colors.navText};
 
-    @media (min-width: ${breakPoints.tablet}) {
-        display: none;
-    }
+  @media (min-width: ${breakPoints.tablet}) {
+    display: none;
+  }
 
-    &:hover {
-        cursor: pointer;
-        background: ${colors.navHighlight};
-    }
+  &:hover {
+    cursor: pointer;
+    background: ${colors.navHighlight};
+    border: 1px solid ${colors.navHighlight};
+  }
+
+  transition: ${transitionSpeed};
 `;
 Container.displayName = 'Container';
 
