8d6c69f4137dd8f6ad0dba0562b8b2b1ff0876e1
Nick Bansal <extreme_wired@msn.com>
2019-07-05 12:33:28 +0100

    updated the props on allblogs

--- src/HomePage/AllBlogs/test.spec.jsx
+++ src/HomePage/AllBlogs/test.spec.jsx
@@ -4,37 +4,56 @@ import AllBlogs from '.';
 
 describe('<AllBlogs />', () => {
   let wrapper;
-  const data = [{
-    _id: '123456',
-    title: 'Test title',
-    edited: false,
-    body: 'Test body',
-    created: new Date('21/10/1988'),
-    image: 'image.jpg',
-    label: 'cats',
-  }];
+  const data = [
+    {
+      _id: '123456',
+      title: 'Test title',
+      edited: false,
+      body: 'Test body',
+      created: new Date('21/10/1988'),
+      image: 'image.jpg',
+      label: 'cats',
+    },
+  ];
+  const props = {
+    handlePageChange: jest.fn(),
+    blogsPerPage: 5,
+    currentPage: 1,
+    loading: false,
+  };
   beforeEach(() => {
-    wrapper = shallow(<AllBlogs data={data} />);
+    wrapper = shallow(<AllBlogs data={data} {...props} />);
   });
   it('Should render a title', () => {
-    expect(wrapper.find('Title').children().text()).toBe('Test title');
+    expect(
+      wrapper
+        .find('Title')
+        .children()
+        .text(),
+    ).toBe('Test title');
   });
   it('Should render an image', () => {
     expect(wrapper.find('Image').exists()).toBe(true);
   });
   it('Should not display a bottom-border on the last element', () => {
-    wrapper = mount(<AllBlogs data={data} />);
+    wrapper = mount(<AllBlogs data={data} {...props} />);
     wrapper.setProps({
-      data: [...data, {
-        _id: '654321',
-        title: 'Test title 2',
-        edited: false,
-        body: 'Test body 2',
-        created: new Date('21/10/1988'),
-        image: 'image.jpg',
-        label: 'cats',
-      }],
+      data: [
+        ...data,
+        {
+          _id: '654321',
+          title: 'Test title 2',
+          edited: false,
+          body: 'Test body 2',
+          created: new Date('21/10/1988'),
+          image: 'image.jpg',
+          label: 'cats',
+        },
+      ],
     });
-    expect(wrapper.find('BlogWrapper').at(1)).toHaveStyleRule('border-bottom', 'none');
+    expect(wrapper.find('BlogWrapper').at(1)).toHaveStyleRule(
+      'border-bottom',
+      'none',
+    );
   });
 });
