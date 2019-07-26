6f9cfd26dec705fce1a461b1d32d7dbadc728bb0
Nick Bansal <extreme_wired@msn.com>
2019-07-26 19:40:03 +0100

    drop zone styled again

--- src/HomePage/CreateModal/Form/index.jsx
+++ src/HomePage/CreateModal/Form/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { func } from 'prop-types';
 import Dropzone from 'react-dropzone';
-import { spacing, colors } from 'Components/StyleGuide';
+import { spacing, colors, breakPoints } from 'Components/StyleGuide';
 import { postSingleBlog } from 'Utils';
 import Buttons from 'Components/Buttons';
 import Completed from './Completed';
@@ -54,6 +54,37 @@ const Error = styled.p`
   align-items: center;
 `;
 
+const Section = styled.section`
+  height: 80px;
+  width: 80%;
+  border-radius: 4px;
+  display: flex;
+  border: 2px dotted;
+  padding: 0 ${spacing.s1};
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DropZoneText = styled.p`
+  font-size: 15px;
+  @media (min-width: ${breakPoints.mobile}) {
+    font-size: 18px;
+  }
+}
+`;
+
+const DZInner = styled.div`
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Click = styled.span`
+  color: ${colors.navText};
+  font-style: italic;
+`;
 
 const Form = ({ openCreate, addBlog }) => {
   const [state, setState] = useState({
@@ -102,14 +133,24 @@ const Form = ({ openCreate, addBlog }) => {
         </Label>
         <Label>
           <Title> Image:</Title>
-          <Dropzone onDrop={file => setState({ ...state, fileInput: file[0] })}>
-            {({ getInputProps }) => (
-              <section>
-                <div>
+          <Dropzone onDrop={(file) => {
+            console.log(file);
+            setState({ ...state, fileDropped: true, fileInput: file[0] });
+          }}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <Section>
+                <DZInner {...getRootProps()}>
                   <input {...getInputProps()} />
-                  <p>Drag and drop some files here, or click to select files</p>
-                </div>
-              </section>
+                  <DropZoneText>
+                    Drag and drop some files here, or
+                    {' '}
+                    <Click>click</Click>
+                    {' '}
+                    to select files
+                  </DropZoneText>
+                </DZInner>
+              </Section>
             )}
           </Dropzone>
         </Label>
