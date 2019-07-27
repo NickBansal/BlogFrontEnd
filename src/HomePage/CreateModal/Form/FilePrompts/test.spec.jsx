f8101f38499187234cb37ce415215df17d3a5c90
Nick Bansal <extreme_wired@msn.com>
2019-07-27 17:06:28 +0100

    more coverage on FilePrompts component

--- src/HomePage/CreateModal/Form/FilePrompts/test.spec.jsx
+++ src/HomePage/CreateModal/Form/FilePrompts/test.spec.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import FilePrompts from '.';
 
 describe('<FilePrompt />', () => {
+  const handleClick = jest.fn();
   const props = {
     notSupported: false,
-    handleClick: jest.fn(),
+    handleClick,
   };
-  it('Renders correct text when file is not supported', () => {
+  it('Should render correct text when file is not supported', () => {
     const { getByTestId } = render(<FilePrompts {...props} />);
     expect(getByTestId('file-dropped')).toHaveTextContent('File selected, please click here to replace file');
   });
-  it('Renders correct text when file is not supported', () => {
+  it('Should render correct text when file is not supported', () => {
     const { getByTestId } = render(<FilePrompts {...props} notSupported />);
     expect(getByTestId('file-dropped')).toHaveTextContent('File type not supported, please click here to replace file');
+  });
+  it('Should fire the handleClick function on the onClick event handler', () => {
+    const { getByTestId } = render(<FilePrompts {...props} notSupported />);
+    expect(handleClick).not.toHaveBeenCalled();
+    fireEvent.click(getByTestId('file-dropped'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
   describe('Renders the correct styling attributes', () => {
     it('file not supported', () => {
