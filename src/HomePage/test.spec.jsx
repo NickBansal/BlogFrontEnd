86b3fd2ded56d63d9a8cbe77674a67aa3a9d7140
Nick Bansal <extreme_wired@msn.com>
2019-08-16 14:19:51 +0100

    added testing for homepage

--- src/HomePage/test.spec.jsx
+++ src/HomePage/test.spec.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { SynchronousPromise } from 'synchronous-promise';
+import { render, wait } from '@testing-library/react';
 import { getAllBlogs } from 'Utils';
 import Homepage from '.';
 
 jest.mock('Utils');
 
-getAllBlogs.mockImplementation(() => SynchronousPromise.resolve(
-  [
-    [{ label: 'Cats', id: 1 }, { label: 'Cats', id: 2 }, { label: 'Dogs', id: 3 }, { label: 'Lions', id: 4 }],
-  ],
-));
-
-describe('<Blog />', () => {
+describe('<Homepage />', () => {
   let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<Homepage />);
-  });
-  afterEach(() => {
-    getAllBlogs.mockClear();
-  });
-  it('Should render a top Navigation component', () => {
-    expect(wrapper.find('TopNavigation').exists()).toBe(true);
+
+  it('Should show an error message when api call is not successful', async () => {
+    getAllBlogs.mockRejectedValue(new Error());
+    wrapper = render(<Homepage />);
+    await wait();
+    expect(wrapper.getByText('Error')).toBeDefined();
   });
 });
